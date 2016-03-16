b8e7732ad0ef1412d768ac61006adaa277254144
Kate Ramsey <kateramsey@live.com>
2016-03-16 14:04:45 -0500

    Inital Commit

---
+++ SQL Practice using an ORM/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL_Practice_using_an_ORM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
