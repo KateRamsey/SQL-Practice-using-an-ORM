2ed023eee85ee949cbd11eb27cb043ef0c0b6805
Kate Ramsey <kateramsey@live.com>
2016-03-16 22:20:39 -0500

    Add SaleAmount class

---
+++ SQL Practice using an ORM/SaleAmount.cs
@@ -0,0 +1,20 @@
+﻿namespace SQL_Practice_using_an_ORM
+{
+    public class SaleAmount
+    {
+        public SaleAmount()
+        {
+
+        }
+        public SaleAmount(decimal pretaxamount)
+        {
+            PreTaxAmount = pretaxamount;
+        }
+        public decimal PreTaxAmount { get; set; }
+
+        public override string ToString()
+        {
+            return $"PreTaxAmount = {PreTaxAmount}";
+        }
+    }
+}
